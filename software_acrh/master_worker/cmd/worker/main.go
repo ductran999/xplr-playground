1a4e5dfe975bcc3d3402d1e0081c2772a3a5bb22
Daniel <74949792+ductran999@users.noreply.github.com>
2026-03-14 18:26:35 +0700

    refactor: worker register flow

--- software_acrh/master_worker/cmd/worker/main.go
+++ software_acrh/master_worker/cmd/worker/main.go
@@ -3,22 +3,16 @@ package main
 import (
 	"context"
 	"log"
+	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	pb "play-ground/software_acrh/master_worker/api/gen/pb/agent/v1"
 	"play-ground/software_acrh/master_worker/internal/worker/app"
 	"play-ground/software_acrh/master_worker/internal/worker/config"
-
-	"google.golang.org/grpc/metadata"
 )
 
 func main() {
-	appCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
 	cfg := config.MustLoad()
 
 	w, err := app.Initialize(cfg)
@@ -27,78 +21,14 @@ func main() {
 	}
 	defer w.Close()
 
+	appCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
 	if err := w.Run(appCtx); err != nil {
 		log.Fatalf("agent start error: %v", err)
 	}
 
-	// StartTunnelLoop(client, "cluster-uuid-123")
-}
-
-func StartTunnelLoop(client pb.AgentServiceClient, clusterID string) {
-	backoff := time.Second * 1
-	maxBackoff := time.Minute * 1
-
-	for {
-		log.Printf("Cluster %s: Attempting to connect to Control Plane...", clusterID)
-
-		// 1. Setup context và metadata
-		md := metadata.Pairs("x-cluster-id", clusterID)
-		ctx := metadata.NewOutgoingContext(context.Background(), md)
-
-		// 2. Open stream
-		stream, err := client.ConnectTunnel(ctx)
-		if err != nil {
-			log.Printf("Connection failed: %v. Retrying in %v", err, backoff)
-			time.Sleep(backoff)
-
-			backoff *= 2
-			if backoff > maxBackoff {
-				backoff = maxBackoff
-			}
-			continue
-		}
-
-		backoff = time.Second * 1
-		log.Println("Tunnel established successfully.")
-
-		handleStream(stream, clusterID)
-
-		log.Println("Stream connection lost. Reconnecting...")
-	}
-}
-
-func handleStream(stream pb.AgentService_ConnectTunnelClient, clusterID string) {
-	done := make(chan struct{})
-
-	// Goroutine A: Listen cmd from server
-	go func() {
-		for {
-			cmd, err := stream.Recv()
-			if err != nil {
-				log.Printf("Recv error: %v", err)
-				close(done)
-				return
-			}
-			log.Printf("Executing command: %s", cmd.Action)
-		}
-	}()
-
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-done:
-			return
-		case <-ticker.C:
-			// Send heartbeat
-			err := stream.Send(&pb.ConnectTunnelRequest{
-				ClusterId: clusterID,
-				Status:    "ONLINE",
-			})
-			if err != nil {
-				log.Printf("Send error: %v", err)
-				return
-			}
-		}
-	}
+	slog.Info("worker is running!")
+	<-appCtx.Done()
+	slog.Info("app shutdown gracefully!")
 }
--- software_acrh/master_worker/internal/worker/app/app.go
+++ software_acrh/master_worker/internal/worker/app/app.go
@@ -2,14 +2,17 @@ package app
 
 import (
 	"context"
+	"log"
 	"log/slog"
 	agentv1 "play-ground/software_acrh/master_worker/api/gen/pb/agent/v1"
 	"play-ground/software_acrh/master_worker/internal/worker/config"
 	"play-ground/software_acrh/master_worker/internal/worker/registration/infra"
 	"play-ground/software_acrh/master_worker/internal/worker/registration/usecase"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
 type WorkerApp struct {
@@ -48,8 +51,7 @@ func (wa *WorkerApp) Run(ctx context.Context) error {
 	if err := wa.registerClusterUC.Execute(ctx); err != nil {
 		return err
 	}
-
-	<-ctx.Done()
+	slog.Info("registration completed successfully!")
 
 	return nil
 }
@@ -61,5 +63,74 @@ func (wa *WorkerApp) Close() {
 
 	if err := wa.Conn.Close(); err != nil {
 		slog.Warn("close grpc connection failed", "error", err)
+	}
+}
+
+func StartTunnelLoop(client agentv1.AgentServiceClient, clusterID string) {
+	backoff := time.Second * 1
+	maxBackoff := time.Minute * 1
+
+	for {
+		log.Printf("Cluster %s: Attempting to connect to Control Plane...", clusterID)
+
+		// 1. Setup context và metadata
+		md := metadata.Pairs("x-cluster-id", clusterID)
+		ctx := metadata.NewOutgoingContext(context.Background(), md)
+
+		// 2. Open stream
+		stream, err := client.ConnectTunnel(ctx)
+		if err != nil {
+			log.Printf("Connection failed: %v. Retrying in %v", err, backoff)
+			time.Sleep(backoff)
+
+			backoff *= 2
+			if backoff > maxBackoff {
+				backoff = maxBackoff
+			}
+			continue
+		}
+
+		backoff = time.Second * 1
+		log.Println("Tunnel established successfully.")
+
+		handleStream(stream, clusterID)
+
+		log.Println("Stream connection lost. Reconnecting...")
+	}
+}
+
+func handleStream(stream agentv1.AgentService_ConnectTunnelClient, clusterID string) {
+	done := make(chan struct{})
+
+	// Goroutine A: Listen cmd from server
+	go func() {
+		for {
+			cmd, err := stream.Recv()
+			if err != nil {
+				log.Printf("Recv error: %v", err)
+				close(done)
+				return
+			}
+			log.Printf("Executing command: %s", cmd.Action)
+		}
+	}()
+
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			// Send heartbeat
+			err := stream.Send(&agentv1.ConnectTunnelRequest{
+				ClusterId: clusterID,
+				Status:    "ONLINE",
+			})
+			if err != nil {
+				log.Printf("Send error: %v", err)
+				return
+			}
+		}
 	}
 }
